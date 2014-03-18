a956e5ef6a3a6954aacf3f925423417644ad1375
ciriosolutions <info@ciriosolutions.com>
2014-03-18 22:29:02 +0100

    Revert d8e752a..a751e89
    This rolls back to commit d8e752a582f3b3649eaf9b64c973fc2795282a54.

---
+++ platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : /Users/miguel/Developer/VeraCofrade14/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
---
+++ platforms/android/ant-build/HelloWorld-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/miguel/developer/veracofrade14/platforms/android/ant-build/HelloWorld-debug-unaligned.apk : \
+/Users/miguel/developer/veracofrade14/platforms/android/ant-build/HelloWorld.ap_ \
+/Users/miguel/developer/veracofrade14/platforms/android/ant-build/classes.dex \
+/Users/miguel/Developer/VeraCofrade14/platforms/android/CordovaLib/ant-build/classes.jar \
